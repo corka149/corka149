2c8a803d58ea2f7629d2fc86ba1405d1a18b93ab
Sebastian Ziemann <corka149@mailbox.org>
2022-03-23 15:47:46 +0100

    Add script for storing elixir extensions (just in case)

---
+++ elixir-vscode-extensions.sh
@@ -0,0 +1,24 @@
+# code --list-extensions | xargs -L 1 echo code --install-extension                                                                                                                                                                                                              
+
+code --install-extension alefragnani.Bookmarks
+code --install-extension donjayamanne.githistory
+code --install-extension eamodio.gitlens
+code --install-extension erlang-ls.erlang-ls
+code --install-extension florinpatrascu.vscode-elixir-snippets
+code --install-extension Gruntfuggly.todo-tree
+code --install-extension humao.rest-client
+code --install-extension JakeBecker.elixir-ls
+code --install-extension maiquitome.elixir-theme
+code --install-extension mitchdenny.ecdc
+code --install-extension ms-azuretools.vscode-docker
+code --install-extension ms-vscode-remote.remote-containers
+code --install-extension ms-vscode.test-adapter-converter
+code --install-extension msaraiva.surface
+code --install-extension oderwat.indent-rainbow
+code --install-extension phoenixframework.phoenix
+code --install-extension PKief.material-icon-theme
+code --install-extension redhat.vscode-xml
+code --install-extension Valentin.beamdasm
+code --install-extension VisualStudioExptTeam.vscodeintellicode
+code --install-extension wmaurer.change-case
+code --install-extension yzhang.markdown-all-in-one
